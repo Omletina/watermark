0f742b2b37938c8e0ff04435cf5b58ef8beb0b4d
urla <urla48@yandex.ru>
2015-05-26 00:20:41 +0300

    Show download file name in custom input

--- app/js/form.js
+++ app/js/form.js
@@ -1,17 +1,24 @@
 $(function () {
     var moveX = $("#moveX").spinner(),
         moveY = $("#moveY").spinner(),
-        moveY = $("#margin-left").spinner(),
-        moveY = $("#margin-bottom").spinner(),
+        marginLeft = $("#margin-left").spinner(),
+        marginBottom = $("#margin-bottom").spinner(),
         opacity = $("#opacity").slider({
             range: "min",
             value: 100,
             orientation: "horizontal"
         });
+
     $(".placement-select__item").on("click", function () {
         var id = $(this).data('select-placement');
 
         $(id).addClass('active').siblings('.position-select').removeClass('active');
         $(this).addClass('active').siblings('.placement-select__item').removeClass('active');
-    })
+    });
+    $('input[type=file]').change(function() {
+        var input = $(this).data('download-file'),
+            filename = $(this).val().split('\\').pop();
+
+        $(input).val( filename ); // Set the value
+    });
 });
